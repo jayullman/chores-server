cb52400b823c65f98f8dbb64115fe37375bd1733
Jay Ullman <jayullman@gmail.com>
2017-07-10 00:04:18 -0700

    Add password validation and password hashing

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,10 @@
 const User = require('../models/user');
 
-
 exports.signup = function(req, res, next) {
   const user = new User({
     email: req.body.email,
-    name: req.body.name
+    name: req.body.name,
+    password: req.body.password
   });
 
 // TODO: Send existing email error to client
@@ -13,16 +13,16 @@ exports.signup = function(req, res, next) {
   // from: https://stackoverflow.com/questions/30593882/how-to-catch-the-error-when-inserting-a-mongodb-document-which-violates-an-uniqu
   user.save(function(err, user) {
     if (err) {
-      
+     
       // if email already exists
       if (err.name === 'MongoError' && err.code === 11000) {
-        console.log('email already exists')
         return res.status(422).send({ error: 'Email is in use' });
 
       // Validation error when required field is not submitted  
-      } else if (err.name === 'ValidationError') {
-        return res.status(422).send({ error: err.message });
+      } else if (err.name === 'ValidationError' && err.errors.password.kind === 'required') {
+        return res.status(422).send({ error: 'No password supplied' });
       }
+      
     } else {
       return res.status(200).send({ message: 'User has been created', user: user });
     }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const connection = require('../services/database');
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   name: String,
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }
 });
+
+// hash password before saving to database
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    })
+  })
+})
 
 module.exports = connection.model('User', userSchema);
